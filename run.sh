d242b49d0bbd412a99bbab417261878ff376f4b5
fepfitra <fitriansyahekaputra@mail.ugm.ac.id>
2023-09-15 12:42:37 +0700

    add attempts

--- run.sh
+++ run.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# Default number of attempts
+NUM_ATTEMPTS=1
+
+# Check if a -r option is provided and set NUM_ATTEMPTS accordingly
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -r|--attempts)
+      NUM_ATTEMPTS="$2"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
 SORTS=('bubble' 'selection' 'insertion' 'quick')
 ARRLENS=(100 200 400 800)
 
@@ -21,10 +39,15 @@ do
     echo $SORT
     for ARRLEN in "${ARRLENS[@]}" 
     do
-      output=$(./"$SORT" $ARRLEN)
-      echo "$ARRLEN length array takes time $output microseconds"
+      echo "Running $SORT $NUM_ATTEMPTS times with array length $ARRLEN"
+      for ((i=1; i<=$NUM_ATTEMPTS; i++)); do
+        output=$(./"$SORT" $ARRLEN)
+        echo "Attempt $i: $output microseconds"
+      done
+      echo -e "\n"
     done
   else
     echo "Executable '$SORT' not found. Compilation may have failed for $SORT."
   fi
 done
+
